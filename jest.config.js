3c2ef0dcbb3c6ce2c0f40ccfda655679d2ed5966
Carlos Nobrega <nobregacarlosjr@gmail.com>
2020-07-26 17:12:41 -0300

    chore: changes on jest config

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,9 @@
 module.exports = {
   roots: ["<rootDir>/src"],
-  collectCoverageFrom: ["<rootDir>/src/**/*.ts"],
+  collectCoverageFrom: ["<rootDir>/src/**/*.spec.ts"],
   coverageDirectory: "coverage",
   coverageProvider: "v8",
   testEnvironment: "node",
-
   transform: {
     ".+\\.ts$": "ts-jest",
   },
