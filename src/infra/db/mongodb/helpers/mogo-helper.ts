998586b160f2a6f17bde514a505a58310b1f1168
Carlos Nobrega <nobregacarlosjr@gmail.com>
2020-08-02 21:14:37 -0300

    refactor: add helper to mongo db connetion

---
+++ src/infra/db/mongodb/helpers/mogo-helper.ts
@@ -0,0 +1,15 @@
+import { MongoClient } from 'mongodb'
+
+export const MongoHelper = {
+  client: null as MongoClient,
+  async connect (uri: string): Promise<void> {
+    this.client = await MongoClient.connect(process.env.MONGO_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    })
+  },
+
+  async close () {
+    this.client.close()
+  }
+}
