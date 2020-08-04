0b46e0f809c2f2737858ad11508969376f0bffa6
Carlos Nobrega <nobregacarlosjr@gmail.com>
2020-08-03 23:07:15 -0300

    test: ensure AccountMongoRepository returns a account on succes

---
+++ src/infra/db/mongodb/account-repository/account.spec.ts
@@ -0,0 +1,30 @@
+import { MongoHelper } from '../helpers/mogo-helper'
+import { AccountMongoRepository } from './accounts'
+
+const makeSut = (): AccountMongoRepository => {
+  return new AccountMongoRepository()
+}
+
+describe('Account Mongo Repository', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL)
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  test('Should return an account on success', async () => {
+    const sut = makeSut()
+    const account = await sut.add({
+      name: 'any_name',
+      email: 'any_mail@mail.com',
+      password: 'any_password'
+    })
+    expect(account).toBeTruthy()
+    expect(account.id).toBeTruthy()
+    expect(account.name).toBe('any_name')
+    expect(account.email).toBe('any_mail@mail.com')
+    expect(account.password).toBe('any_password')
+  })
+})
